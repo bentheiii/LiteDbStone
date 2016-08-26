6e72601ce0123c998a2f9468ef425753e91a7e7b
Ben <Ben@BEN-PC>
2016-08-26 11:29:11 +0300

    initial

---
+++ LiteDbStone/DropAllCollections.cs
@@ -0,0 +1,15 @@
+﻿using LiteDB;
+
+namespace LiteDbStone
+{
+    public static class sropAllCollections
+    {
+        public static void DropAllCollections(this LiteDatabase @this)
+        {
+            foreach (string col in @this.GetCollectionNames())
+            {
+                @this.DropCollection(col);
+            }
+        }
+    }
+}
